aa68eceb5627b1674ec26630233daa30808d1162
Sewer <seweratwork@gmail.com>
2020-05-15 07:04:03 +0100

    Initial Publish
    Add readme.
    
    
    .

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-NoSubtitles
+<div align="center">
+	<h1>No Cutscenes</h1>
+	<img src="https://i.imgur.com/BjPn7rU.png" width="150" align="center" />
+    <br/><br/>
+<b>Id: sonicheroes.utils.nosubtitles</b>
+</div>
+
+# About This Project
+
+An extremely simple [Reloaded II](https://github.com/Reloaded-Project/Reloaded-II) Mod Loader mod that removes all cutscene text.
+
+It's only a few lines of code :)
+
+
+## How to Use
+**A.** Install Reloaded mods as usual. (Extract to mod directory)
+
+**B.** Enable mod and run the game. (This auto-generates the config files: `Config.json`)
+
+**C.** Adjust configuration. (If necessary).
---
+++ sonicheroes.utils.nosubtitles/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Reloaded.Hooks.Definitions;
+using Reloaded.Hooks.Definitions.X86;
+using Reloaded.Mod.Interfaces;
+using Reloaded.Mod.Interfaces.Internal;
+using CallingConventions = Reloaded.Hooks.Definitions.X86.CallingConventions;
+using IReloadedHooks = Reloaded.Hooks.ReloadedII.Interfaces.IReloadedHooks;
+
+namespace sonicheroes.utils.nosubtitles
+{
+    public unsafe class Program : IMod
+    {
+        private IModLoader _modLoader;
+        private IHook<PrintSubtitles> _printSubtitles;
+        
+        public void Start(IModLoaderV1 loader)
+        {
+            _modLoader = (IModLoader)loader;
+
+            /* Your mod code starts here. */
+            _modLoader.GetController<IReloadedHooks>().TryGetTarget(out var hooks);
+            _printSubtitles = hooks.CreateHook<PrintSubtitles>(PrintSubtitlesImpl, 0x00428560).Activate();
+        }
+
+        private int PrintSubtitlesImpl(char* font, string text, IntPtr posX, IntPtr posY, IntPtr e, float f, float g)
+        {
+            return _printSubtitles.OriginalFunction(font, "", posX, posY, e, f, g);
+        }
+
+        /* Mod loader actions. */
+        public void Suspend() { }
+        public void Resume() { }
+        public void Unload() { }
+
+        /*  If CanSuspend == false, suspend and resume button are disabled in Launcher and Suspend()/Resume() will never be called.
+            If CanUnload == false, unload button is disabled in Launcher and Unload() will never be called. */
+        public bool CanUnload()  => false;
+        public bool CanSuspend() => false;
+
+        /* Automatically called by the mod loader when the mod is about to be unloaded. */
+        public Action Disposing { get; }
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        [Function(CallingConventions.Stdcall)]
+        public unsafe delegate int PrintSubtitles(char* font, [MarshalAs(UnmanagedType.LPStr)] string text, IntPtr posX, IntPtr posY, IntPtr e, float f, float g);
+    }
+}
